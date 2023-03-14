845cbc210eeab00bceebf3e0efcb307997d4a132
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-03-14 10:56:55 +0000

    adding api

--- infrastructure/stack.ts
+++ infrastructure/stack.ts
@@ -2,17 +2,26 @@ import {Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {Function as LamdaFunction, Runtime,Code} from 'aws-cdk-lib/aws-lambda'
 import {join} from 'path'
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 
 
 export class SpaceStack extends Stack {
+
+    private api = new RestApi (this,'spaceapi')
     constructor(scope:Construct,id:string,props: StackProps){
         super(scope,id,props)
 
-            new LamdaFunction(this,'hello-lamda',{
+            const helloLambda = new LamdaFunction(this,'hello-lamda',{
             runtime: Runtime.NODEJS_14_X,
             code: Code.fromAsset(join(__dirname,'..','services','hello')),
             handler:'hello.main'
 
         })
+
+        // Hello API integregation:
+            const helloLambdaInt = new LambdaIntegration(helloLambda)
+            const helloLambdaRes = this.api.root.addResource('hello');
+            helloLambdaRes.addMethod('GET',helloLambdaInt)
+
     }
 }
